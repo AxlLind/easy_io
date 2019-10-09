7d0e2337f070b28107225ac1ff3d13e8cefc7903
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2019-10-09 13:16:34 +0200

    Add simple functions for all number types

--- src/input_reader.rs
+++ src/input_reader.rs
@@ -1,4 +1,3 @@
-
 use std::io::{self, Read, Result, Error};
 
 pub struct InputReader {
@@ -7,10 +6,11 @@ pub struct InputReader {
   current_index: usize,
 }
 
+// Constructors
 impl InputReader {
-  fn new() -> Result<InputReader> { InputReader::with_buf_size(1 << 16) }
+  pub fn new() -> Result<InputReader> { InputReader::with_buf_size(1 << 16) }
 
-  fn with_buf_size(buf_size: usize) -> Result<InputReader> {
+  pub fn with_buf_size(buf_size: usize) -> Result<InputReader> {
     let mut input = InputReader {
       buf: vec![0; buf_size],
       bytes_read: 0,
@@ -19,7 +19,72 @@ impl InputReader {
     input.ensure_buffer()?;
     Ok(input)
   }
+}
 
+// Public methods
+impl InputReader {
+  pub fn next_usize(&mut self) -> Result<usize> {
+    self.consume_until(|c| c.is_ascii_digit())?;
+
+    let mut num = 0;
+    loop {
+      let c = self.peek_next();
+      if !c.is_ascii_digit() {
+        return Ok(num);
+      }
+      let digit = c as usize - '0' as usize;
+      num = num * 10 + digit;
+      self.consume()?;
+    }
+  }
+
+  pub fn next_i64(&mut self) -> Result<i64> {
+    self.consume_until(|c| c.is_ascii_digit() || c == '-')?;
+
+    let mut sign = 1;
+    if self.peek_next() == '-' {
+      sign = -1;
+      self.consume()?;
+    }
+    Ok(self.next_usize()? as i64 * sign)
+  }
+
+  pub fn next_f64(&mut self) -> Result<f64> { Ok(self.next_word()?.parse().unwrap()) }
+
+  pub fn next_word(&mut self) -> Result<String> {
+    self.consume_until(|c| c.is_ascii_graphic())?;
+
+    let mut word = String::new();
+    loop {
+      let c = self.peek_next();
+      if c.is_whitespace() {
+        return Ok(word);
+      }
+      word.push(c);
+      self.consume()?;
+    }
+  }
+
+  pub fn next_line(&mut self) -> Result<String> {
+    let mut line = String::new();
+    loop {
+      let c = self.peek_next();
+      if c == '\n' {
+        return Ok(line);
+      }
+      line.push(c);
+      self.consume()?;
+    }
+  }
+
+  pub fn next_u32(&mut self) -> Result<u32> { Ok(self.next_usize()? as u32) }
+  pub fn next_u64(&mut self) -> Result<u64> { Ok(self.next_usize()? as u64) }
+  pub fn next_i32(&mut self) -> Result<i32> { Ok(self.next_i64()?   as i32) }
+  pub fn next_f32(&mut self) -> Result<f32> { Ok(self.next_f64()?   as f32) }
+}
+
+// Private methods
+impl InputReader {
   fn ensure_buffer(&mut self) -> Result<()> {
     if self.current_index == self.bytes_read {
       self.current_index = 0;
@@ -40,59 +105,10 @@ impl InputReader {
   }
 
   fn consume_until<F: Fn(char) -> bool>(&mut self, test: F) -> Result<()> {
-    loop {
-      if test(self.peek_next()) { return Ok(()); }
+    while !test(self.peek_next()) {
       self.consume()?;
     }
-  }
-
-  fn next_u32(&mut self) -> Result<u32> {
-    self.consume_until(|c| c.is_ascii_digit())?;
-    let mut num = 0;
-    loop {
-      match self.peek_next() {
-        '0'..='9' => {
-          let digit = self.peek_next() as u32 - '0' as u32;
-          num = num * 10 + digit;
-        },
-        _ => return Ok(num),
-      };
-      self.consume()?;
-    }
-  }
-
-  fn next_i32(&mut self) -> Result<i32> {
-    self.consume_until(|c| c.is_ascii_digit() || c == '-')?;
-    let mut sign = 1;
-    let mut num = 0;
-    if self.peek_next() == '-' {
-      sign = -1;
-      self.consume()?;
-    }
-
-    loop {
-      match self.peek_next() {
-        '0'..='9' => {
-          let digit = self.peek_next() as i32 - '0' as i32;
-          num = num * 10 + digit;
-        },
-        _ => return Ok(sign * num),
-      };
-      self.consume()?;
-    }
-  }
-
-  fn next_word(&mut self) -> Result<String> {
-    self.consume_until(|c| c.is_ascii_graphic())?;
-    let mut word = String::new();
-    loop {
-      let c = self.peek_next();
-      if c.is_whitespace() {
-        return Ok(word);
-      }
-      word.push(c);
-      self.consume()?;
-    }
+    Ok(())
   }
 }
 
