269956d3ebce2a743c0ad42a7a9d19c5ad08e89b
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2019-10-12 10:13:55 +0200

    Fix bug with reading until EOF

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # InputReader
-This is a fast and easy to use, one-file class for reading numbers and words from either stdin, a file, or any other reader. The main use of this class is in competitive programming.
+This is a fast, easy to use, one-file class for reading numbers and words from either stdin, a file, or any other reader.
 
-Reading particularly numbers from stdin via the regular `io::stdin` class is not very convenient. In competitive programming you want to be able to easily get the next number in the stream or next word since you know the exact format of the input before hand. This class makes that trivial while also being fast.
+The main use of this class is in competitive programming. Reading particularly numbers from stdin via the regular `io::stdin` class is not very convenient. In competitive programming you want to be able to easily get the next number in the stream or next word since you know the exact format of the input before hand. This class makes that trivial while also being fast.
 
 ## Usage
 The simplest use-case for this in competitive programming is to download the file from [here](https://github.com/AxlLind/InputReader/blob/master/src/input_reader.rs). Then simply put it in the same folder as your solution and import it like below. Below is a simple example of using the class:
@@ -14,19 +14,19 @@ use input_reader::InputReader;
 // ...
 
 fn main() -> std::io::Result<()> {
-  // Create a reader from stdin.
-  let mut input = InputReader::new()?;
+  // Create a reader from stdin
+  let mut input = InputReader::new();
 
-  // Create a reader from a file.
+  // ... or from a file.
   let mut input = InputReader::from_file("input.txt")?;
 
-  // Create a reader from any class that implements the Read trait.
-  // In this example from a tcp stream. To do this you explicitly need
-  // to wrap it in a Box, i.e Box::new(object).
-  let tcp_stream = TcpStream::connect("127.0.0.1:34254")?;
+  // ... or from any class that implements the Read trait.
+  // The reader needs to be wrapped in a Box.
+  // In this example from a tcp stream:
+  let tcp_stream = TcpStream::connect("127.0.0.1:34254");
   let mut input = InputReader::from_reader(Box::new(tcp_stream))?;
 
-  // Read numbers and words from the input simply like this.
+  // Read numbers and words from the input source simply like this.
   let x: usize = input.next_usize()?;
   let y: i64 = input.next_i64()?;
   let z: f32 = input.next_f32()?;
@@ -40,17 +40,18 @@ fn main() -> std::io::Result<()> {
 ### Constructors
 ```Rust
 // Constructs an InputReader which reads from stdin.
-InputReader::new() -> Result<InputReader>
+InputReader::new() -> InputReader
 ```
 
 ```Rust
 // Constructs an InputReader which reads from the file at the given path.
+// Note that this returns a result since it will try to open the file.
 InputReader::from_file(path: &str) -> Result<InputReader>
 ```
 
 ```Rust
 // Constructs an InputReader that reads from the given reader.
-InputReader::from_reader(reader: Box<dyn Read>) -> Result<InputReader>
+InputReader::from_reader(reader: Box<dyn Read>) -> InputReader
 ```
 
 ### Reader methods
@@ -59,12 +60,12 @@ The following methods are pretty self-explanatory. They read the next *thing* fr
 ```Rust
 InputReader::next_usize(&mut self) -> Result<usize>
 
-InputReader::next_i8(&mut self) -> Result<i8>
+InputReader::next_i8(&mut self)  -> Result<i8>
 InputReader::next_i16(&mut self) -> Result<i16>
 InputReader::next_i32(&mut self) -> Result<i32>
 InputReader::next_i64(&mut self) -> Result<i64>
 
-InputReader::next_u8(&mut self) -> Result<u8>
+InputReader::next_u8(&mut self)  -> Result<u8>
 InputReader::next_u16(&mut self) -> Result<u16>
 InputReader::next_u32(&mut self) -> Result<u32>
 InputReader::next_u64(&mut self) -> Result<u64>
@@ -76,10 +77,14 @@ InputReader::next_word(&mut self) -> Result<String>
 InputReader::next_line(&mut self) -> Result<String>
 ```
 
-### Instance methods
+### Other instance methods
 ```Rust
-// Changes the internal buffer size of the InputReader. Default: 2^16 bytes
-InputReader::set_buf_size(&mut self, buf_size: usize)
+// Returns Ok(true) if there is more data to be read, Ok(false) otherwise.
+InputReader::has_more(&mut self) -> Result<bool>
+
+// Changes the internal buffer size. Default: 2^16 bytes
+// Will return error if shrinking the buffer will cause data loss.
+InputReader::set_buf_size(&mut self, buf_size: usize) -> Result<()>
 ```
 
 ## :warning: Limitations
--- src/input_reader.rs
+++ src/input_reader.rs
@@ -13,86 +13,87 @@ pub struct InputReader {
   str_buf: String,
 }
 
+// Constructors
 impl InputReader {
-  pub fn new() -> Result<InputReader> {
+  pub fn new() -> InputReader {
     InputReader::from_reader(Box::new( io::stdin() ))
   }
 
   pub fn from_file(path: &str) -> Result<InputReader> {
-    InputReader::from_reader(Box::new( File::open(path)? ))
+    let reader = Box::new( File::open(path)? );
+    Ok(InputReader::from_reader(reader))
   }
 
-  pub fn from_reader(reader: Box<dyn Read>) -> Result<InputReader> {
-    let mut input = InputReader {
+  pub fn from_reader(reader: Box<dyn Read>) -> InputReader {
+    InputReader {
       reader,
       buf: vec![0; 1 << 16],
       bytes_read: 0,
       current_index: 0,
       str_buf: String::with_capacity(1 << 8),
-    };
-    input.ensure_buffer()?;
-    Ok(input)
+    }
   }
+}
 
+// public instance methods
+impl InputReader {
   pub fn set_buf_size(&mut self, buf_size: usize) -> Result<()> {
     if buf_size < self.bytes_read {
-      return io_error!("You cannot shrink the buffer");
+      return io_error!("Data loss while shrinking buffer!");
     }
     self.buf.resize(buf_size, 0);
     Ok(())
   }
 
+  pub fn has_more(&mut self) -> Result<bool> {
+    self.ensure_buffer()?;
+    Ok(self.bytes_read > 0)
+  }
+
   pub fn next_word(&mut self) -> Result<String> {
     self.consume_until(|c| c.is_ascii_graphic())?;
+    self.assert_has_more()?;
 
     self.str_buf.clear();
-    while self.peek().is_ascii_graphic() {
+    while self.has_more()? && self.peek().is_ascii_graphic() {
       self.str_buf.push(self.peek());
-      self.consume()?;
+      self.consume();
     }
     Ok(self.str_buf.clone())
   }
 
   pub fn next_line(&mut self) -> Result<String> {
+    self.assert_has_more()?;
     self.str_buf.clear();
-    while self.peek() != '\n' {
+    while self.has_more()? && self.peek() != '\n' {
       self.str_buf.push(self.peek());
-      self.consume()?;
+      self.consume();
     }
     Ok(self.str_buf.clone())
   }
 
   pub fn next_usize(&mut self) -> Result<usize> {
     self.consume_until(|c| c.is_ascii_digit())?;
+    self.assert_has_more()?;
 
     let mut num = 0;
-    while self.peek().is_ascii_digit() {
+    while self.has_more()? && self.peek().is_ascii_digit() {
       let digit = self.peek() as usize - '0' as usize;
       num = num * 10 + digit;
-      self.consume()?;
+      self.consume();
     }
     Ok(num)
   }
 
   pub fn next_i64(&mut self) -> Result<i64> {
-    let mut sign = 1;
-    loop {
-      self.consume_until(|c| c.is_ascii_digit() || c == '-')?;
-
-      if self.peek() != '-' { break; }
-      self.consume()?;
-
-      if self.peek().is_ascii_digit() {
-        sign = -1;
-        break;
-      }
-    }
+    let sign = self.consume_until_signed_num()?;
     Ok(self.next_usize()? as i64 * sign)
   }
 
   pub fn next_f64(&mut self) -> Result<f64> {
-    self.consume_until(|c| c.is_ascii_digit() || c == '-')?;
-    Ok(self.next_word()?.parse().unwrap())
+    let sign = self.consume_until_signed_num()? as f64;
+    let float: f64 = self.next_word()?.parse().unwrap();
+    Ok(float * sign)
   }
 
   pub fn next_u8(&mut self)  -> Result<u8>  { Ok(self.next_usize()? as u8)  }
@@ -107,27 +108,57 @@ impl InputReader {
   pub fn next_f32(&mut self) -> Result<f32> { Ok(self.next_f64()? as f32) }
 }
 
+// private instance methods
 impl InputReader {
+  fn peek(&self) -> char { self.buf[self.current_index] as char }
+
+  fn consume(&mut self) { self.current_index += 1; }
+
   fn ensure_buffer(&mut self) -> Result<()> {
-    if self.current_index == self.bytes_read {
-      self.current_index = 0;
+    if self.current_index >= self.bytes_read {
       self.bytes_read = self.reader.read(&mut self.buf[..])?;
-      if self.bytes_read == 0 {
-        return io_error!("Could not read more bytes");
-      }
+      self.current_index = 0;
     }
     Ok(())
   }
 
-  fn peek(&self) -> char { self.buf[self.current_index] as char }
-
-  fn consume(&mut self) -> Result<()> {
-    self.current_index += 1;
-    self.ensure_buffer()
+  fn assert_has_more(&mut self) -> Result<()> {
+    if !self.has_more()? {
+      return io_error!("No more input");
+    }
+    Ok(())
   }
 
   fn consume_until<F: Fn(char) -> bool>(&mut self, test: F) -> Result<()> {
-    while !test(self.peek()) { self.consume()?; }
+    while self.has_more()? && !test(self.peek()) {
+      self.consume();
+    }
     Ok(())
   }
+
+  fn consume_until_signed_num(&mut self) -> Result<i64> {
+    let mut sign = 1;
+    while self.has_more()? {
+      self.consume_until(|c| c.is_ascii_digit() || c == '-')?;
+
+      if self.peek() != '-' { break; }
+      self.consume();
+
+      // need to check that the next char after
+      // '-' is actually a digit
+      if self.peek().is_ascii_digit() {
+        sign = -1;
+        break;
+      }
+    }
+    Ok(sign)
+  }
+}
+
+fn main() -> Result<()> {
+  let s = "1 hej -123 abc 123.04".as_bytes();
+  let mut input = InputReader::from_reader(Box::new(s));
+
+  println!("{}\n{}\n{}\n{}", input.next_u8()?, input.next_word()?, input.next_i32()?, input.next_f64()?);
+  Ok(())
 }
